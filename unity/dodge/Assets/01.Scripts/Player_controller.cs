e7cb2736f985e1528be7d9fa25b172c831b5ad93
samoure <mst3050@g.shingu.ac.kr>
2024-03-14 20:07:29 +0900

    game 1-1

---
+++ unity/dodge/Assets/01.Scripts/Player_controller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    private const float V = 0f;
+    public Rigidbody player_rigidbody;
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) == true)
+        {
+            player_rigidbody.AddForce(0f, 0f, speed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) == true)
+        {
+            player_rigidbody.AddForce(0f, 0f, -speed);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            player_rigidbody.AddForce(-speed, 0f, 0f);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            player_rigidbody.AddForce(speed, 0f, 0f);
+        }
+    }
+    public void die()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
+
+        
+
